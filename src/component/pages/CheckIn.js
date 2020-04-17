bf5f81188e330f49c4459929d3d787ce27fa4f35
BrianH-hub <1brianh@gmail.com>
2020-04-17 12:39:18 -0700

    testing buttons

--- src/component/pages/CheckIn.js
+++ src/component/pages/CheckIn.js
@@ -1,4 +1,33 @@
 import React from "react";
-export default function CheckIn(){
-    return(<div>Check in Page</div>)
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
+
+
+export default function CheckIn() {
+    const classes = useStyles();
+    
+  return (
+    <div>
+      Check-in Page
+      <Button variant="contained" color="primary">
+        Check-in
+      </Button>
+
+      <Button className={classes.root}>Hook</Button>
+    </div>
+  );
 }
+
