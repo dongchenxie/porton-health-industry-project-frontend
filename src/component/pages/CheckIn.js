922275bc0bd7deeda9abd50c9a59c7878c12da68
BrianH-hub <1brianh@gmail.com>
2020-04-17 12:51:44 -0700

    sign-in format test

--- src/component/pages/CheckIn.js
+++ src/component/pages/CheckIn.js
@@ -3,9 +3,26 @@ import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+
+
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
     root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
       border: 0,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -13,7 +30,41 @@ const useStyles = makeStyles({
       height: 48,
       padding: '0 30px',
     },
-  });
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
+  }));
+
+
+// export default function CheckIn() {
+//     const classes = useStyles();
+    
+//   return (
+//     <div>
+//       Check-in Page
+//       <Button variant="contained" color="primary">
+//         Check-in
+//       </Button>
+
+//       <Button className={classes.root}>Check-in</Button>
+//     </div>
+//   );
+// }
+
 
 
 export default function CheckIn() {
@@ -22,12 +73,73 @@ export default function CheckIn() {
   return (
     <div>
       Check-in Page
-      <Button variant="contained" color="primary">
-        Check-in
-      </Button>
-
-      <Button className={classes.root}>Hook</Button>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
     </div>
   );
 }
+
+
+// const useStyles = makeStyles((theme) => ({
+
+// }));
+
 
