cfb354448cb255680ed92a8110fe2ff9f3bc8eeb
Vibha Rana <ranavibha02@gmail.com>
2020-04-17 01:08:33 -0700

    few details added

--- src/component/pages/Register.js
+++ src/component/pages/Register.js
@@ -12,13 +12,22 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+  Redirect
+  
+} from "react-router-dom";
+const BASE_URL = "http://localhost:5000/api/user"; //not sure about port number
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Porton Health Check-In Kiosk
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
