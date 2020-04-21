84fa6a896aa3b1daee62c76a25ba0bb722a2fbb3
tomkf <thomaskeithferris@gmail.com>
2020-04-20 18:31:13 -0700

    remove comments

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -34,17 +34,15 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 
-// import LoginWidget from '../component/widgets/loginWidget'
-// import RegisterWidget from '../component/widgets/registerWidget'
-
-//custom components
+//custom components:
 import PrivateRoute from '../component/middleware/PrivateRoute'
 import LoginPage from '../component/pages/Login'
 import NotFoundPage from '../component/pages/NotFoundPage'
 import AuthAPI from "../data/AuthContext"
+// import LoginWidget from '../component/widgets/loginWidget'
+// import RegisterWidget from '../component/widgets/registerWidget'
 
 
-//////////////////////////
 //App styles:
 
 const drawerWidth = 240;
@@ -91,165 +89,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-// }));
-
-////////////////////////////////
-
-
-// export default function AdminRouter() {
-//   //get router path and url
-//   let { path, url } = useRouteMatch();
-//   //material ui build-in style hook
-//   const classes = useStyles();
-//   //state indicate if the user is authenticated
-//   const [auth, setAuth] = React.useState(true);
-//   const authContext =React.useContext(AuthContext)
-//   //state indicate if the profile meun is opened
-//   const [anchorEl, setAnchorEl] = React.useState(null);
-//   const open = Boolean(anchorEl);
-//   React.useEffect(() => {
-//     if(localStorage.getItem('token')){
-//       authContext.setAuthState({
-//         isAuthenticated:true,
-//         token:localStorage.getItem('token')
-
-//       })
-//     }else{
-//       console.log("here")
-//       authContext.setAuthState({
-//         isAuthenticated:false,
-//         token:"123123"
-//       })
-//     }
-//     console.log(authContext.authState.token)
-//     console.log(authContext.authState.isAuthenticated)
-//   },[]);
-//   // const handleLoginChange = (event) => {
-//   //   authContext.setAuthState((prev)=>{
-//   //     return {...prev,isAuthenticated:event.target.checked}});
-//   // };
-
-//   const handleProfileMenu = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleProfileClose = () => {
-//     setAnchorEl(null);
-//   };
-//   const handleSignOut = () =>{
-//       authContext.API.signOut()
-//       handleProfileClose()
-//   }
-//   return (
-//     <Router>
-
-//          <div className={classes.root}>
-//       <AppBar position="static">
-
-
-
-
-//         <Toolbar>
-//           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" className={classes.title}>
-//             Porton Heath Admin Panel
-//           </Typography>
-//           {authContext.authState.isAuthenticated ?(
-//             <div>
-//               <IconButton
-//                 aria-label="account of current user"
-//                 aria-controls="menu-appbar"
-//                 aria-haspopup="true"
-//                 onClick={handleProfileMenu}
-//                 color="inherit"
-//               >
-//                 <AccountCircle />
-//               </IconButton>
-//               <Menu
-//                 id="menu-appbar"
-//                 anchorEl={anchorEl}
-//                 anchorOrigin={{
-//                   vertical: 'top',
-//                   horizontal: 'right',
-//                 }}
-//                 keepMounted
-//                 transformOrigin={{
-//                   vertical: 'top',
-//                   horizontal: 'right',
-//                 }}
-//                 open={open}
-//                 onClose={handleProfileClose}
-//               >
-//                 <MenuItem onClick={handleProfileClose}>Profile</MenuItem>
-//                 <MenuItem onClick={handleSignOut}>Sign out</MenuItem>
-//               </Menu>
-//             </div>
-//           ):(
-//             <div className={classes.flex}>
-//                 {/* <LoginWidget className={classes.btn} auth={authContext.authState.isAuthenticated}/>
-//                 <RegisterWidget className={classes.btn}/> */}
-//             </div>
-//           )}
-//         </Toolbar>
-//       </AppBar>
-//     </div>
-
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to={`${url}/`}>Home(not secure)</Link>
-//             </li>
-//             <li>
-//               <Link to={`${url}/about`}>About(secure)</Link>
-//             </li>
-//             <li>
-//               <Link to={`${url}/users`}>Users(secure)</Link>
-//             </li>
-//           </ul>
-//         </nav>
-//         {/* <FormGroup>
-//         <FormControlLabel
-//           control={<SwitchComponent checked={authContext.authState.isAuthenticated} onChange={handleLoginChange} aria-label="login switch" />}
-//           label={authContext.authState.isAuthenticated ? 'Logout' : 'Login'}
-//         />
-//       </FormGroup> */}
-
-//         {/* A <Switch> looks through its children <Route>s and
-//             renders the first one that matches the current URL. */}
-
-//         <Switch>
-//           <Route exact path={path}>
-//             <Home />
-//           </Route>
-//           <PrivateRoute path={`${path}/about`}>
-//             <About />
-//           </PrivateRoute>
-//           <Route path={`${path}/login`}>
-//             <LoginPage />
-//           </Route>
-//           <PrivateRoute path={`${path}/users`}>
-//             <Users />
-//           </PrivateRoute>
-//           <Route path="*">
-//             <NotFoundPage />
-//           </Route>
-//         </Switch>
-//          <ResponsiveDrawer/> 
-//       </div>
-//     </Router>
-//   );
-// }
 
 
 export default function AdminRouter(props) {
@@ -484,7 +323,7 @@ export default function AdminRouter(props) {
 }
 
 
-//child component, render results in the above: <main className={classes.content}>
+//child components, renders results in the above <main className={classes.content}> tag
 
 function Home() {
   return <h2>Home (not secure data)</h2>;
