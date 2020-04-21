6ab90d0bb6be71168b92d839ce7440ab10cf32cc
tomkf <thomaskeithferris@gmail.com>
2020-04-20 17:36:49 -0700

    refactor

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -44,7 +44,7 @@ import NotFoundPage from '../component/pages/NotFoundPage'
 import AuthAPI from "../data/AuthContext"
 
 
-////////////////////////////////////////////
+//////////////////////////
 //App styles:
 
 const drawerWidth = 240;
@@ -315,23 +315,23 @@ export default function AdminRouter(props) {
        <div className={classes.toolbar} />
        <Divider />
        <List>
-         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+         {['Create Account', 'Account List'].map((text, index) => (
            <ListItem button key={text}>
-             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
            <ListItemText primary={text} />
            </ListItem>
          ))}
        </List>
-       <Divider />
+       <Divider />   
        <List>
-         {['All mail', 'Trash', 'Spam'].map((text, index) => (
+         {['Home', 'About'].map((text, index) => (
            <ListItem button key={text}>
-             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-             <ListItemText primary={text} />
+           <ListItemText primary={text} />
            </ListItem>
          ))}
        </List>
-     </div>
+       <Divider />    
+      
+      </div>
    );
 
 
@@ -474,17 +474,6 @@ export default function AdminRouter(props) {
           </Route>
         </Switch>
       </div>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
       </main>
     </div>
     </Router>
