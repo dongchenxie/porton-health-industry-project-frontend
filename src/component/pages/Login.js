d8c7354e9c6f85fb18ff5bfccd1921f52d1cb5f5
Brian <1brianh@gmail.com>
2020-04-22 10:54:53 -0700

    rebranched login out from Brian Branch

--- src/component/pages/Login.js
+++ src/component/pages/Login.js
@@ -1,53 +1,169 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-    useHistory,
-    useLocation
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
 } from "react-router-dom";
-import AuthContext from "../../data/AuthContext"
-import AuthAPI from "../../data/DataAccessService"
+import AuthContext from "../../data/AuthContext";
+import AuthAPI from "../../data/DataAccessService";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 export default function CheckIn() {
-    let history = useHistory();
-    let location = useLocation();
-    const authContext = React.useContext(AuthContext);
-
-    let { from } = location.state || { from: { pathname: "/" } };
-    let login = async() => {
-        let result =await authContext.API.login("xxx@x.com","password")
-        if(result.status==200){
-            console.log(result)
-            localStorage.setItem("token", result.token)
-            authContext.setAuthState((prev) => {
-            return {
-                ...prev,
-                isAuthenticated: true
-            }
-            })
-            history.replace(from);
-        }else{
-            console.log (result)
-        }  
-    };
-    // React.useEffect(()=>{
-    //     return ()=>{
-    //       if(localStorage.getItem("token")){
-    //           console.log("here")
-    //       console.log("found token in localStorage")
-    //       authContext.setAuthState((prev)=>{
-    //         return {...prev, token:localStorage.getItem("token"),isAuthenticated:true}
-    //       })
-    //       history.replace(from);
-    //     }}
-    //   },[authContext,history,from])
-
-    return (
-        <div>
-            <p>You must log in to view the page at {from.pathname}</p>
-            <button onClick={login}>test Log in with preset account</button>
+  const classes = useStyles();
+  let history = useHistory();
+  let location = useLocation();
+  const authContext = React.useContext(AuthContext);
+  let [username, setUsername] = React.useState("");
+  const handleTextChange = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+  let [password, setPassword] = React.useState("");
+  const handlePasswordChange = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = async () => {
+    let result = await authContext.API.login(`${username}`, `${password}`);
+    //testing purpose "results var"
+    // let result = await authContext.API.login("xxx@x.com", "password");
+    console.log(result);
+    if (result.status == 200) {
+      localStorage.setItem("token", result.token);
+      authContext.setAuthState((prev) => {
+        return {
+          ...prev,
+          isAuthenticated: true,
+        };
+      });
+      history.replace(from);
+    } else {
+      console.log(result);
+    }
+  };
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form
+            className={classes.form}
+              noValidate
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleTextChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePasswordChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              onClick={login}
+              //   type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              // className={classes.submit}
+            >
+              Sign In
+            </Button>
+            {/* <button onClick={login}>test Log in with preset account</button> */}
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-    );
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
+  );
+}
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Porton Health
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
