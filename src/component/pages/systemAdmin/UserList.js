1ed56e0a259558a3fc3363a4478481a029ff14bc
tomkf <thomaskeithferris@gmail.com>
2020-04-28 14:55:55 -0700

    rendering table w/ some errors

--- src/component/pages/systemAdmin/UserList.js
+++ src/component/pages/systemAdmin/UserList.js
@@ -9,13 +9,20 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-//new
+//new: 
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     root: {
       minWidth: 275,
+      width: '100%',
     },
     bullet: {
       display: 'inline-block',
@@ -28,12 +35,35 @@ const useStyles = makeStyles((theme) => ({
     pos: {
       marginBottom: 12,
     },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
+    container: {
+      maxHeight: 440,
     },
-  }));
+  });
+
+
+  //   {
+  //     id: 'population',
+  //     label: 'Population',
+  //     minWidth: 170,
+  //     align: 'right',
+  //     format: (value) => value.toLocaleString('en-US'),
+  //   },
+  //   {
+  //     id: 'size',
+  //     label: 'Size\u00a0(km\u00b2)',
+  //     minWidth: 170,
+  //     align: 'right',
+  //     format: (value) => value.toLocaleString('en-US'),
+  //   },
+  //   {
+  //     id: 'density',
+  //     label: 'Density',
+  //     minWidth: 170,
+  //     align: 'right',
+  //     format: (value) => value.toFixed(2),
+  //   },
+
+
 
 
 export default function Users() {
@@ -43,6 +73,9 @@ export default function Users() {
     const authContext = React.useContext(AuthContext)
     const [users, setUsers] = React.useState(null);
     const [error, setError] = React.useState(null);
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
     React.useEffect(() => {
       const start = async () => {
@@ -66,6 +99,21 @@ export default function Users() {
       start()
     }, [])
 
+    const columns = [
+      { id: 'name', label: 'General Info', minWidth: 170 },
+      { id: 'role', label: 'Role', minWidth: 170 },
+      { id: 'email', label: 'Email', minWidth: 120 },
+      { id: 'action', label: 'Action', minWidth: 150 }]
+
+
+      function createData(name, role, email, action, id ){
+        return { name, role, email, action, id };
+      }
+
+      // const roles = users.map(user => {
+      //   createData()
+      // })
+
     // const renderUsers = (usersArr) => {
     //     let userList =  usersArr.users.map((user, index) =>  ( 
            
@@ -73,63 +121,89 @@ export default function Users() {
     //      return(<div>{userList}</div>)
     // }
 
-  //   const renderUsers = (usersArr) => {
-  //     let userList =  usersArr.users.map((user, index) =>  ( 
-  //       <Card className={classes.root} variant="outlined" key={index}>
-  //       <CardContent>
-  //         <Typography variant="h5" component="h2">
-  //             {user.email}
-  //         </Typography>
-  //         <Typography variant="h5" component="h2">
-  //             {user.role !== "none" ? user.role : ""}
-  //         </Typography>
-  //         <Typography className={classes.pos} color="textSecondary">
-  //           Account created: {user.date}
-  //         </Typography>
-  //       </CardContent>
-  //       <Link to={`${url}/${user._id}`} style={{textDecoration: 'none', color: 'inherit', backgroundColor: 'rgb(104, 251, 234)', borderRadius: '4%'}}> 
-  //         <Button size="small">Client Information and Settings</Button>
-  //       </Link>
-  //       </Card>
-  //      ))
-  //      return(<div>{userList}</div>)
-  // }
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
 
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>item</Paper>
-        </Grid>
-        
-      </React.Fragment>
-    );
+    const renderUsers = (usersArr) => {
+      let userList =  usersArr.users.map((user, index) =>  ( 
+        <Card className={classes.root} variant="outlined" key={index}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+              {user.email}
+          </Typography>
+          <Typography variant="h5" component="h2">
+              {user.role !== "none" ? user.role : ""}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Account created: {user.date}
+          </Typography>
+        </CardContent>
+        <Link to={`${url}/${user._id}`} style={{textDecoration: 'none', color: 'inherit', backgroundColor: 'rgb(104, 251, 234)', borderRadius: '4%'}}> 
+          <Button size="small">Client Information and Settings</Button>
+        </Link>
+        </Card>
+       ))
+       return(<div>{userList}</div>)
   }
 
     return(
         <div> 
+            {console.log(typeof users, users)}
             {error !== null ? error : ""}
-            {/* {users !== null && users !== undefined ? renderUsers(users) : ""} */}
-            <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-      </Grid>
+            {users !== null && users !== undefined ? 
+        <div>
+        <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+        </div>
+         : ""}
        </div>
     ) 
   }
