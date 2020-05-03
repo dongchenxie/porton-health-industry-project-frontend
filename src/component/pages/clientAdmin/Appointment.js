31e35b43a76677927b50dda9c4c4b898b6b8995d
tomkf <thomaskeithferris@gmail.com>
2020-05-02 17:25:56 -0700

    status check

--- src/component/pages/clientAdmin/Appointment.js
+++ src/component/pages/clientAdmin/Appointment.js
@@ -10,6 +10,18 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import CardActions from '@material-ui/core/CardActions';
 import TextField from '@material-ui/core/TextField';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import Popover from '@material-ui/core/Popover';
+import Box from '@material-ui/core/Box';
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
 
 const useStyles = makeStyles({
   root: {
@@ -38,6 +50,8 @@ export default function Appointment() {
   const [error, setError] = React.useState(null);
   const [appoitnment, setAppoitnment] = React.useState(null);
   const [comment, setComment] = React.useState(null);
+  const [value, setValue] = React.useState('female');
+
 
   React.useEffect(() => {
     const start = async () => {
@@ -62,6 +76,7 @@ export default function Appointment() {
     }
     start()
   }, [])
+
 
   const formRow = (label, data) => {
     return (
@@ -105,6 +120,11 @@ export default function Appointment() {
     setComment(e.target.value);
   };
 
+  const handleStatus = (e) => {
+    alert('hello')
+  };
+
+
   const renderAppointment = () => {
     return( 
     <div> <Card className={classes.root} variant="outlined">
@@ -132,6 +152,28 @@ export default function Appointment() {
  </CardContent>
 
  <CardActions style={{display: 'block', width: '50%'}}>   
+ <PopupState variant="popover" popupId="demo-popup-popover">
+      {(popupState) => (
+        <div>
+       <Button size="small" variant="contained" color="primary" style={{marginTop:"2%", marginBottom: '2%'}} {...bindTrigger(popupState)}>Change Appointment Status: </Button>
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+           >
+            <Box p={2}>
+            <StatusChange appoitnment={appoitnment["_id"]} />
+            </Box>
+          </Popover>
+        </div>
+      )}
+    </PopupState>
    <TextField
           multiline={true}
           fullWidth={true}
@@ -149,6 +191,75 @@ export default function Appointment() {
   <Link to={`${path.substring(0, path.length - 4)}`} style={{textDecoration: 'none', color: 'inherit'}}> <Button variant="contained" style={{marginTop: '2%', backgroundColor: 'black', color: 'white'}}> Return to list </Button> </Link>
   </div>)
 }
+
+const updateStatus = () => {
+
+}
+
+const handleChange = (event) => {
+  setValue(event.target.value);
+};
+
+const StatusChange = (appoitnment) => {
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          {/* <FormControl component="fieldset">
+      <FormLabel component="legend">Status:</FormLabel>
+      <FormGroup aria-label="position" row>
+        <FormControlLabel
+          value="top"
+          control={<Radio color="primary" />}
+          label="Checked In"
+          labelPlacement="top"
+        />
+          <FormControlLabel
+          value="top"
+          control={<Radio color="primary" />}
+          label="Pending"
+          labelPlacement="top"
+        />
+          <FormControlLabel
+          value="top"
+          control={<Radio color="primary" />}
+          label="Canceled"
+          labelPlacement="top"
+        />
+
+
+            <Button
+              onClick={updateStatus}
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Confirm
+            </Button>
+            </FormGroup>
+        </FormControl> */}
+        <FormControl component="fieldset">
+      <FormLabel component="legend">Status</FormLabel>
+      <br/>
+      <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}  aria-label="position" row>
+        <FormControlLabel value="female" control={<Radio />} label="Checked In" labelPlacement="top" />
+        <FormControlLabel value="male" control={<Radio />} label="Pending" labelPlacement="top" />
+        <FormControlLabel value="other" control={<Radio />} label="Canceled" labelPlacement="top" />
+      </RadioGroup>
+      <Button
+              onClick={updateStatus}
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Confirm
+            </Button>
+    </FormControl>
+        </div>
+      </Container>
+    </div>
+  );    }
 
     return(
       <div>
