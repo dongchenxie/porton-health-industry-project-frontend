49051fcf41ea381294fb8ba911af016f7d3c46c9
Vibha Rana <ranavibha02@gmail.com>
2020-05-05 20:58:10 -0700

    terminal

--- src/component/pages/TerminalLogin.js
+++ src/component/pages/TerminalLogin.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    useHistory,
-    useLocation,
-  } from "react-router-dom";
-  
-  import AuthContext from "../../data/AuthContext";
-  import AuthAPI from "../../data/DataAccessService";
-
-
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-//import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-=======
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -74,18 +37,13 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
->>>>>>> master
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-<<<<<<< HEAD
-    width: '100%', // Fix IE 11 issue.
-=======
     width: "100%", // Fix IE 11 issue.
->>>>>>> master
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -93,75 +51,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
-export default function TerminalSignIn() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-        Terminal Login
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="token"
-            label="Token"
-            name="token"
-            // autoComplete="email"
-            autoFocus
-          />
-          
-          
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-         Submit The Token
-          </Button>
-          <Grid container>
-            <Grid item xs>
-             
-                <h4>Dont have Token? Ask admin for the help</h4>
-            
-            </Grid>
-            {/* <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid> */}
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-  function Copyright() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-          Porton Health
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-=======
 
 export default function TerminalLogin(props) {
   const classes = useStyles();
@@ -276,5 +165,4 @@ function Copyright() {
       {"."}
     </Typography>
   );
->>>>>>> master
 }
