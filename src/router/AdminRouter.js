056033ed85fdd94435e49a4c1adfe3826f27803f
tomkf <thomaskeithferris@gmail.com>
2020-05-07 18:56:07 -0700

    remove profile page

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -47,7 +47,6 @@ import AppointmentList from '../component/pages/clientAdmin/AppointmentList'
 import Appointment from '../component/pages/clientAdmin/Appointment'
 
 //shared:
-import Profile from '../component/pages/shared/Profile'
 import Copywrite from "../component/pages/shared/Copywrite";
 
 //App styles:
@@ -206,13 +205,6 @@ export default function AdminRouter(props) {
           <ListItemText primary="Home" />
         </ListItem >
       </Link>
-  
-      <Link to={`${url}/profile`} style={{textDecoration: 'none', color: 'inherit'}}>
-        {/* WHAT IS PURPOSE OF THIS FUNCTION? */}
-        <ListItem button onClick={handleProfileClose}>
-          <ListItemText primary="Profile" />
-        </ListItem >
-      </Link>  
 
         <ListItem button onClick={handleSignOut}>
           <ListItemText primary="Sign Out" />
@@ -325,9 +317,6 @@ export default function AdminRouter(props) {
             <Switch>
               <Route exact path={path}>
                 <Home />
-              </Route>
-              <Route path={`${path}/profile`}>
-                <Profile />
               </Route>
               <PrivateRoute path={`${path}/users/:id`}>
                 <UserDetail />
